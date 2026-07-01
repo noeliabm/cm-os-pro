2f9113c8fe7ad320a7d538bc88e23484cbe6eec5
noeliabm <breamorellonoelia@gmail.com>
2026-07-01 09:52:41 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from '@/components/dashboard/Dashboard'
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Dashboard />
+    </div>
+  )
+}
